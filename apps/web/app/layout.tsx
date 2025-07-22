e959d09243eb2ba37aa5d7351beaf7d9cc99c41a
MSGhais <msghais135@gmail.com>
2025-07-22 17:20:31 +0200

    wip

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,8 +3,10 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import ClientLayout from "../components/ui/ClientLayout";
 import React from "react";
+import Script from "next/script";
 
 const geist = Geist({ subsets: ["latin"] });
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
   title: "Focus AFK - Productivity & Focus",
@@ -19,6 +21,29 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          // strategy="lazyOnload"
+          strategy="afterInteractive"
+
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          // strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </head>
       <body className={geist.className}>
         <ClientLayout>
           {children}
